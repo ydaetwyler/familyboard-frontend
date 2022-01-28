63fdc4a116747ed5541b6a3f1e99cc44a636e764
ydaetwyler <ydaetwyler@previon.ch>
2022-01-28 18:58:16 +0100

    improve responsive

--- src/components/Forms/ResetPasswordForm.js
+++ src/components/Forms/ResetPasswordForm.js
@@ -21,7 +21,7 @@ const ResetPasswordForm = () => {
     })
 
     return (
-        <div className="h-2/5 min-h-[430px] w-96 min-w-[300px] bg-white/[.13] absolute -translate-y-2/4 translate-x-2/4 top-2/4 right-2/4 rounded-md backdrop-blur-md border-2 border-white/[.1] shadow-xl shadow-gray-900/[.6] py-12 px-9 before:(p-0, m-0, box-border) after:(p-0, m-0, box-border) font-['Mulish']">
+        <div className="h-2/5 min-h-[480px] w-96 min-w-[300px] max-w-[95%] bg-white/[.13] absolute -translate-y-2/4 translate-x-2/4 top-2/4 right-2/4 rounded-md backdrop-blur-md border-2 border-white/[.1] shadow-xl shadow-gray-900/[.6] py-12 px-9 before:(p-0, m-0, box-border) after:(p-0, m-0, box-border) font-['Mulish']">
             <h3 className="mb-2 text-center text-4xl text-white font-medium leading-9">
                 Set new password
             </h3>
